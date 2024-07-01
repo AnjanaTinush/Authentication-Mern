700e112e3b1c81adbd9c9a1a8cbd6fc8d4416b77
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-01 21:09:39 +0530

    create header section

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -14,7 +14,34 @@ const Header = () => {
 
     return (
         <header className='header'>
-           
+            <nav>
+                <div className='logo' onClick={goHome}>
+                    <FaHome size={35} />
+                    <span>AUTH:Z</span>
+                </div>
+
+                <ul className='home-links'>
+                    <li className='--flex-center'>
+                        <FaUserCircle size={20} />
+                        <p className='--color-white'>Hi, Anjana |</p>
+                    </li>
+                    <li>
+                        <button className='--btn --btn-primary'>
+                            <Link to="/login">Login</Link>
+                        </button>
+                    </li>
+                    <li>
+                        <NavLink to="/profile" className={activelink}>
+                            Profile
+                        </NavLink>
+                    </li>
+                    <li>
+                        <button className='--btn --btn-secondary'>
+                            <Link to="/login">Logout</Link>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
