8a63bf6c1e417024092ab204787f3d532b05a7b9
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-02 20:21:52 +0530

    create register page with validation

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,63 +1,74 @@
 import React, { useState } from 'react'
 import Card from '../../components/card/Card'
 import styles from "./auth.module.scss"
-import {BiLogIn} from 'react-icons/bi'
+import { BiLogIn } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
+import PasswordInput from "../../components/passwordInput/passwordInput"
 
 const Login = () => {
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const handleInputChange = () => {};
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
 
-    const loginUser = ()  => {};
+    const loginUser = (e) => {
+        e.preventDefault();
+        // Logic to login user               
+    };
 
-  return (
-    <div className={`container ${styles.auth}`}>
-        <Card>
-            <div className={styles.form}>
-         <div className='--flex-center'>
-           <BiLogIn size={35} color="#999"/>
-         </div>
-         <h2>Login</h2>
-         <div className='--flex-center'>
-            <button className='--btn --btn-google'>
-             Login With Google
-            </button>
-         </div>
-         <br/>
-         <p className='--text-center --fw-bold'>or</p>
-         
-        <form onSubmit={loginUser}>
-            <input 
-            type='email' 
-            placeholder='Email' 
-            required 
-            name='email' 
-            value={email} 
-            onChange={handleInputChange}
-            />
-            <input 
-            type='password' 
-            placeholder='Password' 
-            required 
-            name='password' 
-            value={password} 
-            onChange={handleInputChange}
-            />
-            <button type='submit' className='--btn --btn-primary --btn-block'>
-                Login</button>
-        </form>
-          <Link to='/forgot'>Forgot Password</Link>
-          <span className={styles.register}>
-            <Link to="/">Home</Link>
-            <p> &nbsp; Don't have an account? &nbsp;</p>
-            <Link to='/register'>Register</Link>
-          </span>
-         </div>
-        </Card>
-    </div>
-  )
+    return (
+        <div className={`container ${styles.auth}`}>
+            <Card>
+                <div className={styles.form}>
+                    <div className='--flex-center'>
+                        <BiLogIn size={35} color="#999" />
+                    </div>
+                    <h2>Login</h2>
+                    <div className='--flex-center'>
+                        <button className='--btn --btn-google'>
+                            Login With Google
+                        </button>
+                    </div>
+                    <br />
+                    <p className='--text-center --fw-bold'>or</p>
+                    
+                    <form onSubmit={loginUser}>
+                        <input
+                            type='email'
+                            placeholder='Email'
+                            required
+                            name='email'
+                            value={email}
+                            onChange={handleInputChange}
+                        />
+                        <PasswordInput
+                            placeholder="Password"
+                            required
+                            name="password"
+                            value={password}
+                            onChange={handleInputChange}
+                        />
+                        <button type='submit' className='--btn --btn-primary --btn-block'>
+                            Login
+                        </button>
+                    </form>
+                    <Link to='/forgot'>Forgot Password</Link>
+                    <span className={styles.register}>
+                        <Link to="/">Home</Link>
+                        <p> &nbsp; Don't have an account? &nbsp;</p>
+                        <Link to='/register'>Register</Link>
+                    </span>
+                </div>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
