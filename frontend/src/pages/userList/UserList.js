306a35e4e4829a6ee4677e23bf450b5df6539b06
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-07-03 12:31:09 +0530

    create all users table

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './UserList.scss'
 import PageMenu from '../../components/pageMenu/PageMenu'
 import UserStats from '../../components/userStats/UserStats'
+import Search from '../../components/search/Search'
+import { FaTrashAlt } from 'react-icons/fa'
 
 const UserList = () => {
   return (
@@ -9,6 +11,53 @@ const UserList = () => {
         <div className='container'>
             <PageMenu/>
             <UserStats/>
+
+            <div className='user-list'>
+                <div className='table'>
+
+                
+                <div className='--flex-between'>
+              <span>
+                <h3>All Users</h3>
+                </span> 
+                <span>
+                    <Search/>
+                </span> 
+                </div>
+                {/*Table */}
+                <table>
+                <thead>
+                  <tr>
+                     <th>S/N</th>
+                     <th>Name</th>
+                     <th>Email</th>
+                     <th>Role</th>
+                     <th>Change Role</th>
+                     <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Anjana</td>
+                    <td>Anjana@gmail.com</td>
+                    <td>Admin</td>
+                    <td>
+                        ChangeRole
+                    </td>
+                    <td>
+                        <span>
+                            <FaTrashAlt size={20} color='red'/>
+                        </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+                </div>
+              
+            </div>
+
+
         </div>
     </section>
   )
